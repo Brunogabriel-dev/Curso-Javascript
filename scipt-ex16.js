64f7cc2d18e08820424ff2f9e2c930f3182c48eb
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-02-23 21:25:33 -0300

    Inclusão de texto

---
+++ scipt-ex16.js
@@ -0,0 +1,12 @@
+// quando o usuário clicar nos links internos dp site,
+// adicione a classe ativo ao item ckicado e remova dos
+// demais intes caso eles possuam a mesma. Previna
+// o comportamento padrão desses links
+
+
+// Selecione todos os elementos do site começando a partir do body
+// ao clique mostre exatamente quais elementos estão sendo clicado
+
+
+// Utilizando o código anterior, ao invés de mostrar no console,
+// remova o elemento que está sendo clicado, o método remove()
