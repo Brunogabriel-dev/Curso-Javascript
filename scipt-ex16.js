e7805497d208957664c0428f298462f32961eee0
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-02-23 21:32:58 -0300

    Inclusão de texto

--- scipt-ex16.js
+++ scipt-ex16.js
@@ -4,9 +4,12 @@
 // o comportamento padrão desses links
 
 
-// Selecione todos os elementos do site começando a partir do body
+// Selecione todos os elementos do site começando a partir do body,
 // ao clique mostre exatamente quais elementos estão sendo clicado
 
 
 // Utilizando o código anterior, ao invés de mostrar no console,
-// remova o elemento que está sendo clicado, o método remove()
+// remova o elemento que está sendo clicado, o método remove() remove um elemento
+
+
+// Se o usuário clicar na tecla (t), aumenta todo o texto do site.
