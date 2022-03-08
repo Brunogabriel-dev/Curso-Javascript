a58fcb69e97d3cf7a23bafc82c662c8ed5e0c040
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-07 21:54:42 -0300

    Inclusão de texto

---
+++ numeros-e-operadores/script.js
@@ -0,0 +1,14 @@
+
+var frase = 'Isso é um teste';
++frase; // NaN
+-frase; // NaN
+
+var idade = '28';
++idade; // 28 (número)
+-idade; // -28 (número)
+console.log(+idade + 5); // 33
+
+var possuiFaculdade = true;
+console.log(+posssuiFaculdade); // 1
+
+
