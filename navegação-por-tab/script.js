565777dd4eac5d8053a9eecf9774eec1723eea45
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-08 21:54:58 -0300

    Inclusão de texto

---
+++ navegação-por-tab/script.js
@@ -0,0 +1,13 @@
+const tabmenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+function activeTab(index) {
+  tabContent.forEach((section) => {
+    section.classList.add('ativo');
+  });
+  tabContent[index].classList.add('ativo');
+}
+
+tabmenu.forEach((itemMenu, index) => {
+  itemMenu.addEventListener('click',)
+});
