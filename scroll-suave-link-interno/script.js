e396fafbd000aa27cc24e200f639029a8ea59fde
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-09 21:58:20 -0300

    Inclusão de texto

--- scroll-suave-link-interno/script.js
+++ scroll-suave-link-interno/script.js
@@ -47,12 +47,15 @@ function scrolltoSection(event) {
   event.preventDefault();
   const href = event.currentTarget.getAttribute('href');
   const section = document.querySelector(href);
-  const topo = section.offsetTop;
+  
 
-  window.scrollTo({
-    top: topo,
-    behavior: 'smooth',
-  });
+  
+  // forma alternativa
+  // const topo = section.offsetTop;
+  // window.scrollTo({
+  // top: topo,
+  // behavior: 'smooth',
+  // });
 }
 
 linksInternos.forEach((link) => {
