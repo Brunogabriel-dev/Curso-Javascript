30a38b7e53a7bf18bc467da725f0439717c35cc8
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-09 21:27:10 -0300

    Inclusão de texto

--- accordion-list/script.js
+++ accordion-list/script.js
@@ -21,14 +21,22 @@ tabmenu.forEach((itemMenu, index) => {
 }
 initTabNav();
 
+function initAccordion() {
 const accordionList = document.querySelectorAll('.js-accordion dt');
+const activeClass = 'ativo';
+if(accordionList.length) {
+accordionList[0].classList.add(activeClass);
+accordionList[0].nextElementSibling.classList.add(activeClass);
 
 function activeAccordion() {
-  this.classList.add('ativo');
-  this.nextElementSibling.classList.add('ativo');
+  this.classList.toggle(activeClass);
+  this.nextElementSibling.classList.toggle(activeClass);
   console.log(this);
 }
 
 accordionList.forEach((item) => {
   item.addEventListener('click', activeAccordion);
-})
+});
+}
+}
+initAccordion();
