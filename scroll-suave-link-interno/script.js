3753a593e8d3257790da06542418f38042fd869d
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-09 21:48:00 -0300

    Inclusão de texto

---
+++ scroll-suave-link-interno/script.js
@@ -0,0 +1,56 @@
+function initTabNav() {
+const tabmenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+if(tabmenu.length && tabContent.lenght) {
+tabContent[0].classLista.add('ativo');
+
+function activeTab(index) {
+  tabContent.forEach((section) => {
+    section.classList.add('ativo');
+  });
+  tabContent[index].classList.add('ativo');
+}
+
+tabmenu.forEach((itemMenu, index) => {
+  itemMenu.addEventListener('click',() => {
+    activeTab(index);
+  });
+});
+}
+}
+initTabNav();
+
+function initAccordion() {
+const accordionList = document.querySelectorAll('.js-accordion dt');
+const activeClass = 'ativo';
+if(accordionList.length) {
+accordionList[0].classList.add(activeClass);
+accordionList[0].nextElementSibling.classList.add(activeClass);
+
+function activeAccordion() {
+  this.classList.toggle(activeClass);
+  this.nextElementSibling.classList.toggle(activeClass);
+  console.log(this);
+}
+
+accordionList.forEach((item) => {
+  item.addEventListener('click', activeAccordion);
+});
+}
+}
+initAccordion();
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function scrolltoSection(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute('href');
+  const section = document.querySelector(href);
+  console.log(section);
+
+}
+
+linksInternos.forEach((link) => {
+  link.addEventListener('click', scrolltoSection);
+})
