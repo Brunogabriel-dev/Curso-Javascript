f3ba4df3b29c9c647deb63b97495001f4388c447
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-10 23:04:58 -0300

    Inclusão de texto

--- constructor-function1/script.js
+++ constructor-function1/script.js
@@ -1,5 +1,8 @@
-function Carro() {
-
+function Carro(marcaAtribuida, preco) {
+ this.marca = marcaAtribuida;
+ this.preco = 2000;
 }
 
 const honda = Carro();
+const fiat = new Carro();
+fiat.marca = 'Fiat'; 
