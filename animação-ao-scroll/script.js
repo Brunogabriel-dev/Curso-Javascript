4e820079c46b82a0cf83d5c05ca4a215eb603bb5
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-10 21:25:01 -0300

    Inclusão de texto

--- animação-ao-scroll/script.js
+++ animação-ao-scroll/script.js
@@ -70,7 +70,13 @@
   const section = document.querySelectorAll('.js-scroll');
 
   function animaScroll() {
-    console.log('sim');
+    section.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      if(sectionTop < 0) {
+        section.classList.add('ativo');
+      }
+    })
   }
 
-  
+  window.addEventListener('scroll', animaScroll);
+
