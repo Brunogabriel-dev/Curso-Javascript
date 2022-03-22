34bc672f7d85d662a5397d90703f06bd947c393d
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-21 22:38:37 -0300

    Inclusão de texto

---
+++ exercícios/script-ex18.js
@@ -0,0 +1,27 @@
+// Transforme o objeto abaixo em uma Contructor Function
+const pessoa = {
+  nome: 'Nome pessoa',
+  idade: 0,
+  andar() {
+    console.log(this.nome + ' andou');
+  }
+}
+
+function Pessoa(nome, idade) {
+  this.nome = nome;
+  this.idade = idade;
+  this.andar = function(){
+    console.log(nome + 'Andar');
+  }
+}
+// Crie 3 pessoas, João - 20 anos,
+// Maria - 25 anos, bruno - 15 anos
+
+
+// Crie uma Constructor Function (Dom) para manipulação
+// de listas de elementos do dom. Deve conter as seguintes
+// propriedades e métodos:
+//
+// elements, retorna NodeList com os elementos selecionados
+// addClass(classe), adiciona a classe a todos os elementos
+// removeClass(classe), remove a classe a todos os elementos
