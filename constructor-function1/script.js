48ae97916c5dd9f1e6a15fdc160f7aa42752739f
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-21 22:03:31 -0300

    Inclusão de texto

--- constructor-function1/script.js
+++ constructor-function1/script.js
@@ -1,7 +1,18 @@
-const Dom = {
-  seletor: 'li',
-  element() {
-    return document.querySelector(this.seletor);
+// const Dom = {
+  // seletor: 'li',
+  // element() {
+    // return document.querySelector(this.seletor);
+  // },
+  // ativar() {
+    // this.element().classList.add('ativar');
+  // }
+// }
 
+function Dom(seletor) {
+  this.element = function() {
+    return document.querySelector(seletor);
+  },
+  this.ativar = function() {
+    this.element().classList.add('ativar');
   }
 }
