cd98afbfe098892409edaa421f76f4f5e7072c9e
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-21 22:19:35 -0300

    Inclusão de texto

--- constructor-function1/script.aula2.js
+++ constructor-function1/script.aula2.js
@@ -11,8 +11,14 @@
 function Dom(seletor) {
   this.element = function() {
     return document.querySelector(seletor);
-  },
-  this.ativar = function() {
-    this.element().classList.add('ativar');
+  }
+  this.ativar = function(classe) {
+    this.element().classList.add(classe);
   }
 }
+
+const li = new Dom('li');
+const ul = new Dom('ul');
+
+const lastLi = new Dom('li:last-child');
+lastLi.ativar('ativar');
